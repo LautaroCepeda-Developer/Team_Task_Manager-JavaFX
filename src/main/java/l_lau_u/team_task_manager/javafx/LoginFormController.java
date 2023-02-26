e7327d4787f4430fa84190bd03c94d8f0849979b
Lautaro Cepeda <LautaroCepeda.Developer@gmail.com>
2023-02-26 07:00:53 -0300

    Initializing_Repository

---
+++ src/main/java/l_lau_u/team_task_manager/javafx/LoginFormController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package l_lau_u.team_task_manager.javafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author l_lau_u
+ */
+public class LoginFormController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module l_lau_u.team_task_manager.javafx {
+    requires javafx.controls;
+    requires java.base;
+    exports l_lau_u.team_task_manager.javafx;
+}
